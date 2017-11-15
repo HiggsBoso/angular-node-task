259516a1eb4601c32d1ec1d65d82ce8e4a7540e4
unknown <higgsboso@yandex.ru>
2017-11-15 22:12:16 +0400

    Add setup for mongolab

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,12 @@
 let mongoose = require('mongoose');
 
+const dbuser = 'guest';
+const dbpassword = 'db_pass';
+
+const mongodbURI = 'mongodb://<dbuser>:<dbpassword>@ds163595.mlab.com:63595/product-db';
+
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/ProductApp', {
+mongoose.connect(mongodbURI || 'mongodb://localhost:27017/ProductApp', {
   useMongoClient: true
 });
 
--- server/server.js
+++ server/server.js
@@ -9,7 +9,6 @@ const { Product } = require('./models/product');
 let app = express();
 const port = process.env.PORT || 3000;
 
-
 app.use(express.static(__dirname + './../dist'));
 app.use(bodyParser.json());
 
